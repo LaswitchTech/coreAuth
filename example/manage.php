bca9424544a8ce82bb5c248ff3a63b4eb201de18
Louis Ouellet <louis_ouellet@hotmail.com>
2024-07-18 14:25:20 -0400

    Changed the src for JQuery to the official CDN

--- example/manage.php
+++ example/manage.php
@@ -71,7 +71,7 @@ $Header = ucfirst($Page);
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
     <title><?= $Header ?> Management</title>
-    <script src="/vendor/components/jquery/jquery.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
   </head>
   <body class="h-100 w-100">
